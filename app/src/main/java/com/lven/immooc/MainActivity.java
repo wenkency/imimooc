a4ee0d2274f62ab4d0634ad3b8a4d6dc253ab642
liufuwen <liufuwen@car-house.net>
2020-04-01 08:40:31 +0800

    first commit

---
+++ app/src/main/java/com/lven/immooc/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lven.immooc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
